71d5cd59f36fe64e9e686db382c8be0f3fdd37c9
zonanjaro86 <zonanjaro86@gmail.com>
2019-03-19 18:03:42 +0900

    スタート・ストップボタン追加

--- js/dig.js
+++ js/dig.js
@@ -1,6 +1,20 @@
 import {log, shuffle} from './util.js';
 import {startTimer, stopTimer} from './timer.js';
 
+const Cursor = class {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        document.getElementById(`${this.x}_${this.y}`).classList.add('cursor');
+    }
+    move(x, y) {
+        document.getElementById(`${this.x}_${this.y}`).classList.remove('cursor');
+        this.x = x;
+        this.y = y;
+        document.getElementById(`${this.x}_${this.y}`).classList.add('cursor');
+    }
+}
+
 // 初期状態
 const width_cell = 5;
 const height_cell = 5;
@@ -13,7 +27,41 @@ const [sx, sy] = [width_cell, height_cell];
 const stock = [];
 stock.push({x: sx, y: sy});
 
+let currentStep = 0;
+const next = () => {
+    currentStep++;
+    if (currentStep > 1) {
+        currentStep = 0;
+    }
+}
+
+let x, y;
+let direction = null;
+
+const dxdy = {
+    0: {label: '北',dx: 0, dy: -1},
+    1: {label: '西',dx: -1, dy: 0},
+    2: {label: '東',dx: 1, dy: 0},
+    3: {label: '南',dx: 0, dy: 1},
+}
+let cursor;
+
 const init = () => {
+    const fragment = document.createDocumentFragment();
+
+    const start_button = document.createElement('button');
+    start_button.innerText = 'start';
+    start_button.setAttribute('id', 'btn_start');
+    start_button.addEventListener('click', start);
+    fragment.appendChild(start_button);
+
+    const stop_button = document.createElement('button');
+    stop_button.innerText = 'stop';
+    stop_button.setAttribute('id', 'btn_stop');
+    stop_button.addEventListener('click', stop);
+    stop_button.disabled = true;
+    fragment.appendChild(stop_button);
+
     const table = document.createElement('table');
     table.style.width = `${cell_size * width}px`;
     table.style.height = `${cell_size * height}px`;
@@ -29,11 +77,15 @@ const init = () => {
         });
         table.appendChild(tr);
     })
-    document.body.appendChild(table);
+    fragment.appendChild(table);
 
     const ul = document.createElement('ul');
     ul.setAttribute('id', 'log');
-    document.body.appendChild(ul);
+    fragment.appendChild(ul);
+
+    document.body.appendChild(fragment);
+
+    cursor = new Cursor(sx, sy);
 };
 
 const dig = (x, y) => {
@@ -42,93 +94,70 @@ const dig = (x, y) => {
     tgt.classList.remove('wall');
 }
 
-const Cursor = class {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        document.getElementById(`${this.x}_${this.y}`).classList.add('cursor');
-    }
-    move(x, y) {
-        document.getElementById(`${this.x}_${this.y}`).classList.remove('cursor');
-        this.x = x;
-        this.y = y;
-        document.getElementById(`${this.x}_${this.y}`).classList.add('cursor');
-    }
+
+const stop = () => {
+    document.getElementById('btn_start').disabled = false;
+    document.getElementById('btn_stop').disabled = true;
+    stopTimer();
 }
 
-
-const main = () => {
-    init();
-    
-    let currentStep = 0;
-    const next = () => {
-        currentStep++;
-        if (currentStep > 1) {
-            currentStep = 0;
+const start = () => {
+    document.getElementById('btn_start').disabled = true;
+    document.getElementById('btn_stop').disabled = false;
+    startTimer(loop, 500);
+}
+   
+let loop_cnt = 1;
+const loop = () => {
+    if (currentStep == 0) {
+        if (stock.length == 0) {
+            stopTimer();
+            log(`${loop_cnt}:\tもう掘れるところが無い！完了！`);
+            dig(1,0);
+            dig(width - 2, height - 1);
+            return;
         }
-    }
+        const {x:_x, y:_y} = stock.pop();
+        [x, y] = [_x, _y];
+        cursor.move(x, y);
     
-    let x, y;
-    let direction = null;
-    
-    const dxdy = {
-        0: {label: '北',dx: 0, dy: -1},
-        1: {label: '西',dx: -1, dy: 0},
-        2: {label: '東',dx: 1, dy: 0},
-        3: {label: '南',dx: 0, dy: 1},
-    }
-    const cursor = new Cursor(sx, sy);
-    
-    dig(sx, sy);
-    let loop_cnt = 1;
-    const loop = () => {
-        if (currentStep == 0) {
-            if (stock.length == 0) {
-                stopTimer();
-                log(`${loop_cnt}:もう掘れるところが無いよ`);
-                return;
-            }
-            const {x:_x, y:_y} = stock.pop();
-            [x, y] = [_x, _y];
-            cursor.move(x, y);
-        
-            const directions = shuffle([0, 1, 2, 3]);
-            for (let i = 0; i < 4; i++) {
-                direction = directions[i];
-                const {dx, dy} = dxdy[direction];
-                if (y + dy * 2 < height && x + dx * 2 < width
-                    && y + dy * 2 >= 0 && x + dx * 2 >= 0
-                    && maze[y + dy * 2][x + dx * 2] == 1) {
-                        break;
-                } else {
-                    direction = null;
-                }
-            }
-            if (direction !== null) {
-                const {label} = dxdy[direction]
-                log(`${loop_cnt}:${label}を掘るよ`);
+        const directions = shuffle([0, 1, 2, 3]);
+        for (let i = 0; i < 4; i++) {
+            direction = directions[i];
+            const {dx, dy} = dxdy[direction];
+            if (y + dy * 2 < height && x + dx * 2 < width
+                && y + dy * 2 >= 0 && x + dx * 2 >= 0
+                && maze[y + dy * 2][x + dx * 2] == 1) {
+                    break;
             } else {
-                log(`${loop_cnt}:掘れるところが無い`);
+                direction = null;
             }
-            next();
-        } else if (currentStep == 1) {
-            if (direction !== null) {
-                const {dx, dy} = dxdy[direction];
-                log(`${loop_cnt}:掘った`);
-                dig(x + dx, y + dy);
-                dig(x + dx * 2, y + dy * 2);
-                stock.push({x, y});
-                stock.push({x: x + dx * 2, y: y + dy * 2});
-            }　else {
-                log(`${loop_cnt}:戻るよ`);
-            }
-            next();
         }
-        loop_cnt++;
+        if (direction !== null) {
+            const {label} = dxdy[direction]
+            log(`${loop_cnt}:\t${label}を掘るよ`);
+        } else {
+            log(`${loop_cnt}:\t掘れるところが無い`);
+        }
+        next();
+    } else if (currentStep == 1) {
+        if (direction !== null) {
+            const {dx, dy} = dxdy[direction];
+            log(`${loop_cnt}:\t掘った`);
+            dig(x + dx, y + dy);
+            dig(x + dx * 2, y + dy * 2);
+            stock.push({x, y});
+            stock.push({x: x + dx * 2, y: y + dy * 2});
+        }　else {
+            log(`${loop_cnt}:\t戻るよ`);
+        }
+        next();
     }
-    startTimer(loop, 200);
+    loop_cnt++;
 }
-main();
+
+init();
+dig(sx, sy);
 
 
 
